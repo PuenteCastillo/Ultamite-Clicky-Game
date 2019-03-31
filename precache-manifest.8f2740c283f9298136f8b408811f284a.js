d579b1ef9c52da55a3e54e2db3cb7ab2b7169653
R2D2 <jp@Joses-MBP.attlocal.net>
2019-03-31 12:24:04 -0700

    Updates

---
+++ precache-manifest.8f2740c283f9298136f8b408811f284a.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = [
+  {
+    "revision": "b6f49138c5e98d872914",
+    "url": "/Ultamite-Clicky-Game/static/js/runtime~main.863f9e6a.js"
+  },
+  {
+    "revision": "7bce05c0d151edeaa6fc",
+    "url": "/Ultamite-Clicky-Game/static/js/main.ebc4aca4.chunk.js"
+  },
+  {
+    "revision": "2e88a627cf5416981f4f",
+    "url": "/Ultamite-Clicky-Game/static/js/2.2ba74eb8.chunk.js"
+  },
+  {
+    "revision": "7bce05c0d151edeaa6fc",
+    "url": "/Ultamite-Clicky-Game/static/css/main.d92abce0.chunk.css"
+  },
+  {
+    "revision": "61d1bd970c19fee86cbd513e060801ee",
+    "url": "/Ultamite-Clicky-Game/index.html"
+  }
+];
